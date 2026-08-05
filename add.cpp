0d7016cfe9f7bd29146d92ec7cd008f251e49c40
sarthak's <sarthakpatel2344@gmail.com>
2026-08-05 13:43:05 +0800

    initial commit

---
+++ add.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int add(int num1, int num2) {
+int  sum=num1+num2;
+    return sum;
+}
+
+int main()
+ {
+
+int a=6;
+int b=5;
+ 
+cout<<add(a,b)<<endl;
+
+
+return 0;
+
+ }
+
+
+
---
+++ sqrt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+     cout<<sqrt(37)<<endl;
+     cout<<pow(20,2)<<endl;
+
+     int ans = sqrt(37);
+     cout<<ans<<endl;
+
+     double doubleans = sqrt(37);
+     cout<<doubleans<<endl;
+
+    return 0;
+}
